126be14dab4537f9dfe6b3bf1582160574c01b25
deen john <dj.itbhu@gmail.com>
2017-04-11 15:42:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://test:test@ds157980.mlab.com:57980/addressnode');
+//mongodb://<dbuser>:<dbpassword>@ds157980.mlab.com:57980/addressnode
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+	firstname: String,
+	lastname: String,
+	address: String
+});
+
+var Person = mongoose.model('Person', personSchema);
+
+var john = Person({
+  firstname: 'John',
+  lastname: 'Doe',
+  address: '555 Main St.'
+});
+
+// save the user
+john.save(function(err) {
+  if (err) {
+		console.log('got error');
+		throw err;
+	}
+  console.log('person saved!');
+});
+
+var jane = Person({
+  firstname: 'Jane',
+  lastname: 'Doe',
+  address: '555 Main St.'
+});
+
+// save the user
+jane.save(function(err) {
+  if (err) throw err;
+
+  console.log('person saved!');
+});
+
+var apiController = require('./controllers/apiController');
+var htmlController = require('./controllers/htmlController');
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.use('/', function (req, res, next) {
+	console.log('Request Url:' + req.url);
+	
+	// get all the users
+	Person.find({}, function(err, users) {
+		if (err) throw err;
+		
+		// object of all the users
+		console.log(users);
+	});
+	
+	next();
+});
+
+htmlController(app);
+
+apiController(app);
+
+app.listen(port);
